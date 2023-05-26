1be9a6204bb1c387fef1b0c9d4db2c4646918fbb
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-05-26 16:32:26 +0700

    commit learn

--- CrudNodeJS/ContentView.swift
+++ CrudNodeJS/ContentView.swift
@@ -5,17 +5,86 @@
 //  Created by Putut Yusri Bahtiar on 26/05/23.
 //
 
+import Foundation
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct Item: Codable {
+    let id: Int
+    let name: String
+}
+
+func getItems(completion: @escaping ([Item]?) -> Void) {
+    guard let url = URL(string: "http://localhost:3000/api/items") else {
+        completion(nil)
+        return
+    }
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if let error = error {
+            print("Error: \(error.localizedDescription)")
+            completion(nil)
+            return
         }
-        .padding()
+        
+        guard let data = data else {
+            completion(nil)
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            let items = try decoder.decode([Item].self, from: data)
+            completion(items)
+        } catch {
+            print("Error decoding response: \(error.localizedDescription)")
+            completion(nil)
+        }
+    }.resume()
+}
+
+func deleteItem(_ item: Item, items: Binding<[Item]>) {
+    guard let url = URL(string: "http://localhost:3000/api/items/\(item.id)") else {
+        return
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "DELETE"
+    
+    URLSession.shared.dataTask(with: request) { data, response, error in
+        if let error = error {
+            print("Error: \(error.localizedDescription)")
+            return
+        }
+        
+        if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 {
+            // Delete successful
+            DispatchQueue.main.async {
+                items.wrappedValue.removeAll { $0.id == item.id }
+            }
+        } else {
+            print("Error deleting item")
+        }
+    }.resume()
+}
+
+struct ContentView: View {
+    @State private var items: [Item] = []
+    
+    var body: some View {
+        List(items, id: \.id) { item in
+            Text(item.name)
+                .onTapGesture {
+                    deleteItem(item, items: $items)
+                }
+        }
+        .onAppear {
+            getItems { fetchedItems in
+                DispatchQueue.main.async {
+                    self.items = fetchedItems ?? []
+                }
+            }
+        }
     }
 }
 
