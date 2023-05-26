b4915ee9de2a632e5634b843ef358ce9a5d4a2f0
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-05-26 15:48:46 +0700

    Initial Commit

---
+++ CrudNodeJS/CrudNodeJSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrudNodeJSApp.swift
+//  CrudNodeJS
+//
+//  Created by Putut Yusri Bahtiar on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CrudNodeJSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
